9d44c7b96c676732a33ef35e3c17fc702dded1be
Dawekis <3331687892@qq.com>
2024-04-14 13:04:28 +0800

    update

--- DOS/Start/myfile.lua
+++ DOS/Start/myfile.lua
@@ -24,9 +24,130 @@ local close = mainFrame:addButton()
 :setForeground(colors.red)
 :onClick(function (self,event,click,x,y)
     if(event == "mouse_click") and (click == 1) then
-        mainFrame:require()
+        mainFrame:close()
     end
 end)
+
+--新建文件夹
+local menu_file = mainFrame:addButton()
+:setText("NewFile")
+:setPosition(1,2)
+:setSize(9,1)
+:setBackground(colors.white)
+:setForeground(colors.black)
+:onClick(function (self,event,click,x,y)
+    if(event == "mouse_click") and (click == 1) then
+        local pId = id
+        id = id + 1
+        local NewFile = mainFrame:addMovableFrame()
+        :setSize(40,14)
+        :setPosition(math.random(2, 12),math.random(2, 8))
+
+        local w,h = NewFile.getSize()
+
+        NewFile:addLabel()
+        :setSize(w, 1)
+        :setBackground(colors.black)
+        :setForeground(colors.white)
+        :setText("NewFile")
+
+        local str = NewFile:addTextfield()
+        :setPosition(1,2)
+        :setSize(40,1)
+        :setForeground(colors.black)
+        :setBackground(colors.white)
+        :addLine("Please input new file name in here.")
+
+        NewFile:addButton()
+        :setPosition(19,8)
+        :setSize(2,1)
+        :setForeground(colors.black)
+        :setBackground(colors.red)
+        :setText("OK")
+        :onClick(function ()
+            local str = str:getLines()
+            shell.run("mkdir",str[1])
+            NewFile:close()
+        end)
+
+        NewFile:addButton()
+        :setSize(1, 1)
+        :setText("X")
+        :setBackground(colors.black)
+        :setForeground(colors.red)
+        :setPosition(w, 1)
+        :onClick(function()
+            NewFile:remove()
+            processes[pId] = nil
+        end)
+    processes[pId] = NewFile
+    return NewFile
+    end
+end)
+
+ --新建lua&nfp&txt
+ local menu_run = mainFrame:addButton()
+ :setText("NewLua&Nfp&Txt")
+ :setPosition(10,2)
+ :setSize(15,1)
+ :setBackground(colors.white)
+ :setForeground(colors.black)
+ :onClick(function (self,event,click,x,y)
+     if(event == "mouse_click") and (click == 1) then
+         local pId = id
+         id = id + 1
+         local Newrun = mainFrame:addMovableFrame()
+         :setSize(40,14)
+         :setPosition(math.random(2, 12),math.random(2, 8))
+ 
+         local w,h = Newrun.getSize()
+ 
+         Newrun:addLabel()
+         :setSize(w, 1)
+         :setBackground(colors.black)
+         :setForeground(colors.white)
+         :setText("NewLua&Nfp")
+ 
+         local str = Newrun:addTextfield()
+         :setPosition(1,2)
+         :setSize(40,1)
+         :setForeground(colors.black)
+         :setBackground(colors.white)
+         :addLine("Please input new file name in here.")
+ 
+         Newrun:addButton()
+         :setPosition(19,8)
+         :setSize(2,1)
+         :setForeground(colors.black)
+         :setBackground(colors.red)
+         :setText("OK")
+         :onClick(function ()
+             local str = str:getLines()
+             if string.find(str,"lua") ~= nil or string.find(str,"txt") ~= nil
+             then
+                shell.run("edit",str[1])
+                Newrun:close()
+             elseif string.find(str,"nfp") ~= nil
+             then
+                shell.run("paint",str[1])
+                Newrun:close()
+             end
+         end)
+ 
+         Newrun:addButton()
+         :setSize(1, 1)
+         :setText("X")
+         :setBackground(colors.black)
+         :setForeground(colors.red)
+         :setPosition(w, 1)
+         :onClick(function()
+             Newrun:remove()
+             processes[pId] = nil
+         end)
+     processes[pId] = Newrun
+     return Newrun
+     end
+ end)
 
 local mainscreen = mainFrame:addFrame()
 :setPosition(1,3)
@@ -46,7 +167,6 @@ local filelistname = mainscreen:addList()
 :setBackground(colors.lightBlue)
 
 local filelist = fs.list(".../")
-
 for i,j in pairs(filelist) do
     if fs.isDir(j) then
         filelistname:addItem(j,colors.lightBlue,colors.black)
@@ -107,20 +227,20 @@ local function openfile(title)
     local pId = id
     id = id + 1
     local miniscreen = mainFrame:addFrame()
-    :setBackground(colors.white)
+    :setBackground(colors.lightBlue)
     :setSize(51,19)
     :setPosition(1,1)
     
     local w,h = miniscreen.getSize()
 
     local minifilelist = miniscreen:addList()
-    :setBackground(colors.white)
+    :setBackground(colors.lightBlue)
     :setForeground(colors.black)
-    :setPosition(1,2)
+    :setPosition(1,3)
     :setSize(w*50/51,h*18/19)
 
     local miniscrollbar = miniscreen:addScrollbar()
-    :setPosition(w,2)
+    :setPosition(w,3)
     :setSize(1,h*18/19)
     :onChange(function (self,_,value)
     minifilelist:setOffset(0,value-1)
@@ -129,15 +249,137 @@ local function openfile(title)
     miniscreen:addPane()
     :setSize(w,1)
     :setBackground(colors.black)
+    miniscreen:addPane()
+    :setSize(w,2)
+    :setBackground(colors.white)
 
     miniscreen:addLabel()
-    :setPosition(2,1)
+    :setPosition(1,1)
     :setSize(w, 1)
     :setBackground(colors.black)
     :setForeground(colors.white)
-    :setText(title or "New Program")
-    
+    :setText(title)
 
+     --新建lua&nfp&txt
+ local menu_run = miniscreen:addButton()
+ :setText("NewLua&Nfp&Txt")
+ :setPosition(10,2)
+ :setSize(15,1)
+ :setBackground(colors.white)
+ :setForeground(colors.black)
+ :onClick(function (self,event,click,x,y)
+     if(event == "mouse_click") and (click == 1) then
+         local pId = id
+         id = id + 1
+         local Newrun = miniscreen:addMovableFrame()
+         :setSize(40,14)
+         :setPosition(math.random(2, 12),math.random(2, 8))
+ 
+         local w,h = Newrun.getSize()
+ 
+         Newrun:addLabel()
+         :setSize(w, 1)
+         :setBackground(colors.black)
+         :setForeground(colors.white)
+         :setText("NewLua&Nfp")
+ 
+         local str = Newrun:addTextfield()
+         :setPosition(1,2)
+         :setSize(40,1)
+         :setForeground(colors.black)
+         :setBackground(colors.white)
+         :addLine("Please input new file name in here.")
+ 
+         Newrun:addButton()
+         :setPosition(19,8)
+         :setSize(2,1)
+         :setForeground(colors.black)
+         :setBackground(colors.red)
+         :setText("OK")
+         :onClick(function ()
+             local str = str:getLines()
+             if string.find(str[1],"lua") ~= nil or string.find(str[1],"txt") ~= nil
+             then
+                shell.run("edit",usepath.."/"..str[1])
+                Newrun:close()
+             elseif string.find(str,"nfp") ~= nil
+             then
+                shell.run("paint",usepath.."/"..str[1])
+                Newrun:close()
+             end
+         end)
+ 
+         Newrun:addButton()
+         :setSize(1, 1)
+         :setText("X")
+         :setBackground(colors.black)
+         :setForeground(colors.red)
+         :setPosition(w, 1)
+         :onClick(function()
+             Newrun:remove()
+             processes[pId] = nil
+         end)
+     processes[pId] = Newrun
+     return Newrun
+     end
+ end)
+    
+    --新建文件夹
+local menu_file = miniscreen:addButton()
+:setText("NewFile")
+:setPosition(1,2)
+:setSize(9,1)
+:setBackground(colors.white)
+:setForeground(colors.black)
+:onClick(function (self,event,click,x,y)
+    if(event == "mouse_click") and (click == 1) then
+        local pId = id
+        id = id + 1
+        local NewFile = miniscreen:addMovableFrame()
+        :setSize(40,14)
+        :setPosition(math.random(2, 12),math.random(2, 8))
+
+        local w,h = NewFile.getSize()
+
+        NewFile:addLabel()
+        :setSize(w, 1)
+        :setBackground(colors.black)
+        :setForeground(colors.white)
+        :setText("NewFile")
+
+        local str = NewFile:addTextfield()
+        :setPosition(1,2)
+        :setSize(40,1)
+        :setForeground(colors.black)
+        :setBackground(colors.white)
+        :addLine("Please input new file name in here.")
+
+        NewFile:addButton()
+        :setPosition(19,8)
+        :setSize(2,1)
+        :setForeground(colors.black)
+        :setBackground(colors.red)
+        :setText("OK")
+        :onClick(function ()
+            local str = str:getLines()
+            shell.run("mkdir",usepath.."/"..str[1])
+            NewFile:close()
+        end)
+
+        NewFile:addButton()
+        :setSize(1, 1)
+        :setText("X")
+        :setBackground(colors.black)
+        :setForeground(colors.red)
+        :setPosition(w, 1)
+        :onClick(function()
+            NewFile:remove()
+            processes[pId] = nil
+        end)
+    processes[pId] = NewFile
+    return NewFile
+    end
+end)
     miniscreen:addButton()
         :setSize(1, 1)
         :setText("X")
@@ -151,7 +393,7 @@ local function openfile(title)
     local filelist = fs.list(usepath)
     for i,j in pairs(filelist) do
         if fs.isDir(usepath) then
-            minifilelist:addItem(j,colors.white,colors.black)
+            minifilelist:addItem(j,colors.lightBlue,colors.black)
         end
     end
     minifilelist:onSelect(function (self,event,item)
