47aba084e67945d4d8106db83a31d0bd6c35f6a6
Dawekis <3331687892@qq.com>
2024-04-29 16:17:50 +0800

    update

--- DOS/System/Dawekis'tools/curvefitting.lua
+++ DOS/System/Dawekis'tools/curvefitting.lua
@@ -7,7 +7,7 @@ local str_1 = {}
 local str_2 = {}
 local str_3 = {}
 local str_4 = {}
-for i in io.lines("DOS/System/Dawekis'tools/UserData/Curve_fitting_config.txt") do
+for i in io.lines("DOS/UserData/Curve fitting/Curve_fitting_config.txt") do
     str_1[j] = i
     j = j+1
 end
@@ -25,30 +25,7 @@ end)
 :onLoseFocus(function(self)
     self:setPosition(-28)
 end)
-sidebar:addLabel():setText("Enter decimals,do not enter"):setPosition(3,13):setForeground(colors.black)
-sidebar:addLabel():setText(" fractions and letters."):setPosition(0,14):setForeground(colors.black)
-sidebar:addLabel():setText("Data points number:"):setPosition(2,2)
-local pnum = cat.Basalt.Textfield(sidebar,colors.white,colors.black,str_1,21,2,9,1)
-sidebar:addLabel():setText("Zoom ratio:"):setPosition(2,4)
-local zoom = cat.Basalt.Textfield(sidebar,colors.white,colors.black,str_2,21,4,9,1)
-sidebar:addLabel():setText("X-axis : Y-axis:"):setPosition(2,6)
-local xy = cat.Basalt.Textfield(sidebar,colors.white,colors.black,str_3,21,6,9,1)
-sidebar:addLabel():setText("Up limit:"):setPosition(2,8)
-local uplimit = cat.Basalt.Textfield(sidebar,colors.white,colors.black,str_4,21,8,9,1)
-cat.Basalt.Button_Text(sidebar,14,11,"OK",colors.red,colors.black,function ()
-    local j = 1
-    local updata = io.open("DOS/System/Dawekis'tools/UserData/Curve_fitting_config.txt","w")
-    io.output(updata)
-    local str = {}
-    str[1] = pnum:getLine(1)
-    str[2] = zoom:getLine(1)
-    str[3] = xy:getLine(1)
-    str[4] = uplimit:getLine(1)
-    for i,j in pairs(str) do
-        io.write(tostring(j).."\n")
-    end
-    io.close(updata)
-end)
+
 local mainscreen = mainFrame:addFrame()
 :setPosition(2,3)
 :setSize(49,16)
@@ -76,13 +53,38 @@ end)
 local aGraph = mainscreen:addGraph():setGraphColor(colors.blue):setBackground(colors.white):setGraphSymbol("")
 :setSize(51,19)
 
-mainFrame:onEvent(function(event, side, channel, replyChannel, message, distance)
+sidebar:addLabel():setText("Enter decimals,do not enter"):setPosition(3,13):setForeground(colors.black)
+sidebar:addLabel():setText(" fractions and letters."):setPosition(0,14):setForeground(colors.black)
+sidebar:addLabel():setText("Data points number:"):setPosition(2,2)
+local pnum = cat.Basalt.Textfield(sidebar,colors.white,colors.black,str_1,21,2,9,1)
+sidebar:addLabel():setText("Zoom ratio:"):setPosition(2,4)
+local zoom = cat.Basalt.Textfield(sidebar,colors.white,colors.black,str_2,21,4,9,1)
+sidebar:addLabel():setText("X-axis : Y-axis:"):setPosition(2,6)
+local xy = cat.Basalt.Textfield(sidebar,colors.white,colors.black,str_3,21,6,9,1)
+sidebar:addLabel():setText("Up limit:"):setPosition(2,8)
+local uplimit = cat.Basalt.Textfield(sidebar,colors.white,colors.black,str_4,21,8,9,1)
+cat.Basalt.Button_Text(sidebar,14,11,"OK",colors.red,colors.black,function ()
+    local j = 1
+    local updata = io.open("DOS/UserData Curve fitting/Curve_fitting_config.txt","w")
+    io.output(updata)
+    local str = {}
+    str[1] = pnum:getLine(1)
+    str[2] = zoom:getLine(1)
+    str[3] = xy:getLine(1)
+    str[4] = uplimit:getLine(1)
+    for i,j in pairs(str) do
+        io.write(tostring(j).."\n")
+    end
+    io.close(updata)
+end)
+
+basalt.onEvent(function(event, side, channel, replyChannel, message, distance)
     local min = 999999
     local max = 0
     aGraph:setSize((tonumber(xy:getLine(1)) or 3.1)*16*(tonumber(zoom:getLine(1)) or 1),16*(tonumber(zoom:getLine(1)) or 1))
     :setMaxEntries(tonumber(pnum:getLine(1)) or 10)
     :setMaxValue(tonumber(uplimit:getLine(1)) or 100)
-    for i in io.lines("DOS/System/Dawekis'tools/UserData/Data.txt") do
+    for i in io.lines("DOS/UserData/Curve fitting/Data.txt") do
         aGraph:addDataPoint(tonumber(i))
         if max < tonumber(i) then
             max = tonumber(i)
--- test.lua
+++ test.lua
@@ -1,13 +1,6 @@
-require("math")
-local i = 0
-while true do
-  shell.run("rm","DOS/System/Dawekis'tools/UserData/Data.txt")
-  for j = i,10+i do
-    local file = io.open("DOS/System/Dawekis'tools/UserData/Data.txt","a")
-    io.output(file)
-    io.write(string.format("%.2f",tostring(1+math.cos(j))).."\n")
-    io.close(file)
-  end
-  i = i + 1
-  sleep(0.001)
-end
+local basalt = require(".../DOS.script.basalt")
+local main = basalt.createFrame()
+basalt.onEvent(function(event, side, channel, replyChannel, message, distance)
+        basalt.debug("Message received: ")
+end)
+basalt.autoUpdate()
